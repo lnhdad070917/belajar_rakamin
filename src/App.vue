3f8bd4dc96154b1d13abdce279926cf6497c6b89
DIDIK ADI DARMAWAN <109228277+lnhdad070917@users.noreply.github.com>
2023-08-09 20:33:34 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">BTPN Syariah</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li>
+            <a @click="dilihat = 'PostinganList'">Postingan</a>
+          </li>
+          <li>
+            <a @click="dilihat = 'buat-postingan'">Buat Postingan</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <input type="text" v-model="name" />
+    <h4>Name: {{ name }}</h4>
+    <div class="container">
+      <component :is="dilihat"></component>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostinganList from "./components/Postingan.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PostinganList,
+  },
+  data() {
+    return {
+      name: "",
+      dilihat: "PostinganList",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Postingan.vue
@@ -0,0 +1,38 @@
+<template id="PostinganList">
+  <div>
+    <h1>Postingan</h1>
+    <div class="list-group">
+      <a
+        href="#"
+        class="list-group-item clearfix"
+        v-for="(post, index) in posts"
+        :key="index"
+      >
+        {{ post }}
+        <span class="pull-right">
+          <button class="btn btn-xs btn-info">
+            <span class="fa fa-edit"></span>
+          </button>
+          <button class="btn btn-xs btn-warning">
+            <span class="fa fa-trash"></span>
+          </button>
+        </span>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "PostinganList",
+  data() {
+    return {
+      posts: [
+        "Membuat Dynamic Component",
+        "Api Laravel 5.5",
+        "HTML Fundamental",
+        "Otentikasi Dengan Laravel 5.5",
+      ],
+    };
+  },
+};
+</script>
